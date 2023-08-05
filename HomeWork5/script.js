5b362fea13626d730541c850ca64e1526b62b45f
Sergey Rodenko <rodenkosv@gmail.com>
2023-08-05 12:35:28 +0700

    added Javascript code

---
+++ HomeWork5/script.js
@@ -0,0 +1,113 @@
+// задание 1
+const numbers = {
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7,
+}
+
+for (const key in numbers) {
+    if (numbers[key] >= 3) {
+        console.log(numbers[key]);
+    }
+}
+
+// задание 2
+const post = {
+    author: "John", // вывести этот текст
+    postId: 23,
+    comments: [
+        {
+            userId: 10,
+            userName: "Alex",
+            text: "lorem ipsum",
+            rating: {
+                likes: 10,
+                dislikes: 2, // вывести это число
+            },
+        },
+        {
+            userId: 5, // вывести это число
+            userName: "Jane",
+            text: "lorem ipsum 2", // вывести этот текст
+            rating: {
+                likes: 3,
+                dislikes: 1,
+            },
+        },
+    ],
+};
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
+
+// задание 3
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+for (const key in products) {
+    products[key].price = products[key].price - products[key].price * 0.15;
+}
+
+
+// задание 4
+const products4 = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ],
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: [],
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg",
+        ],
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+
+// нет решения 
+
+
+// задание 5
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+
+const mix = [];
+
+for (let i = 0; i < en.length; i++) {
+    if (en.length === ru.length) {
+        mix[en[i]] = ru[i];
+    }
+}
+console.log(mix);
+
